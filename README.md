b061378233e9d8500a9179c2cda244bbb8a8697a
FireStreaker2 <michaelccc2019@outlook.com>
2023-06-24 12:53:56 -0700

    small improvements

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# CustomEmbed
- Custom embed maker for discord.
+![CustomEmbed](https://socialify.git.ci/FireStreaker2/CustomEmbed/image?description=1&forks=1&issues=1&language=1&name=1&owner=1&pulls=1&stargazers=1&theme=Dark)
+
+# About
+CustomEmbed was designed for users to make custom embeds for discord, as implied from the name. 
+
+# Usage
+Base URL: ``custom-embed.firestreaker2.gq``
+| Parameters   | Type    | Description                                  |
+| -------------|---------|----------------------------------------------|
+| Title        | String  | Title of the embed                           |
+| Color        | Integer | Color of the embed (in hex form, ex. 000000) |
+| Description  | String  | Description of the embed                     |
+| Image        | String  | URL of the image                             |
+| ImageBig     | Boolean | Whether or not the image is big              |
+
+# Selfhosting
+If you would like to selfhost this project, you may.
+```bash
+$ git clone https://github.com/FireStreaker2/CustomEmbed.git
+$ cd CustomEmbed
+$ npm i
+$ npm start
+```
+
+# License
+<a href="https://github.com/FireStreaker2/CustomEmbed/blob/main/LICENSE">MIT</a>
--- index.js
+++ index.js
@@ -3,30 +3,27 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send(`<script>window.location.href = "https://github.com/FireStreaker2/CustomEmbed";</script>`);
 });
 
 app.get("/embed", (req, res) => {
-    // possible parameters: title, title hyperlink, color, description, image, image size (bool), footer text
+    // check readme for more info on parameters
     const title = req.query.title;
-    const titleHyperlink = req.query.titleHyperlink;
     const color = req.query.color;
     const description = req.query.description;
     const image = req.query.image;
     const imageBig = req.query.imageBig;
-    const footer = req.query.footer;
     res.send(`
     <!DOCTYPE HTML>
     <html>
         <head>
             <title>${title}</title>
             
-            <meta property="og:title" content="${title}" />
-            <meta property="og:url" content="${titleHyperlink}" />
-            <meta property="og:description" content="${description}" />
-            <meta name="theme-color" content="#${color}">
-            <meta property="og:image" content="${image}" />
-            ${imageBig && '<meta name="twitter:card" content="summary_large_image">'}
+            <meta property="og:title" content="${title ? title : "No Title Provided"}" />
+            <meta property="og:description" content="${description ? description : "No Description Provided"}" />
+            <meta name="theme-color" content="#${color ? color : "000000"}">
+            <meta property="og:image" content="${image ? image : ""}" />
+            ${imageBig ? '<meta name="twitter:card" content="summary_large_image">' : "<!-- this image is small -->"}
 
         </head>
 
